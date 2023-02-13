2b49ca6d3a41a850e43457699942eaf1d10ac125
toofanjhala <jhalatoofan@gmail.com>
2023-02-13 13:48:28 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Header } from "./components/layout/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+    <Header/>
+    
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from "./header.module.css"
+
+export const Header = () => {
+    return (
+        <React.Fragment >
+            <header className={classes.header}>
+                <h2>FOOD APP</h2>
+                <button>Cart</button>
+            </header>
+            <div className={classes.mainimage}>
+                <img src="https://as2.ftcdn.net/v2/jpg/02/86/17/89/1000_F_286178925_8zk89O9uC5JJVPvqhvBMUpaRxp8AFXzD.jpg" alt="not found"></img>
+            </div>
+        </React.Fragment>
+    )
+}
