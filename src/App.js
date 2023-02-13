81f8fd5b2f22503f21f204fc3f4bec29980528cc
toofanjhala <jhalatoofan@gmail.com>
2023-02-13 14:33:34 +0530

    meals summary and avilability normal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import Meals from "./components/meals/Meal";
 import { Header } from "./components/layout/Header";
 
 function App() {
   return (
     <React.Fragment>
     <Header/>
+    <main>
+      <Meals/>
+    </main>
     
     </React.Fragment>
   );
